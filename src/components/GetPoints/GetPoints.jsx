51b6464aad553794182ce88bf53bdfa25e50b858
mickey-vee <171717823+mickey-vee@users.noreply.github.com>
2024-11-06 11:14:05 -0500

    Added styling and logic to highlight button when winner is chosen

--- src/components/GetPoints/GetPoints.jsx
+++ src/components/GetPoints/GetPoints.jsx
@@ -3,50 +3,51 @@ import "./GetPoints.scss";
 
 const GetPoints = ({ playerNames, getPoints, getRound, getWinner }) => {
   const [points, setPoints] = useState(Array(playerNames.length).fill(""));
+  const [winner, setWinner] = useState("");
 
   const submitPoints = (e) => {
     e.preventDefault();
-
     getPoints(points.map(Number));
     setPoints(Array(playerNames.length).fill(""));
-
-    // Calculate the next round number
     getRound((prevRound) => prevRound + 1);
-  };
-
-  const calculatePoints = (e) => {
-    e.preventDefault();
   };
 
   return (
     <>
       <form id="points-form" onSubmit={submitPoints} className="points-form">
-        {playerNames.map((player, index) => (
-          <div key={index}>
-            <label name={player}>
-              {player} cards left:
-              <input
-                className="points-input"
-                name={player}
-                type="number"
-                value={points[index]}
-                onChange={(e) => {
-                  const newPoints = [...points];
-                  newPoints[index] = e.target.value;
-                  setPoints(newPoints);
+        {playerNames.map((player, index) => {
+          // Conditionally applying a special class for the winner
+          const isWinner = player === winner;
+
+          return (
+            <div key={index}>
+              <label name={player}>
+                {player} cards left:
+                <input
+                  className="points-input"
+                  name={player}
+                  type="number"
+                  value={points[index]}
+                  onChange={(e) => {
+                    const newPoints = [...points];
+                    newPoints[index] = e.target.value;
+                    setPoints(newPoints);
+                  }}
+                />
+              </label>
+              <button
+                className={isWinner ? "winner-button" : ""}
+                type="button"
+                onClick={() => {
+                  getWinner(player);
+                  setWinner(player);
                 }}
-              />
-            </label>
-            <button
-              type="button"
-              onClick={(e) => {
-                getWinner(player);
-              }}
-            >
-              Round Winner
-            </button>
-          </div>
-        ))}
+              >
+                Round Winner
+              </button>
+            </div>
+          );
+        })}
         <button type="submit">Enter points</button>
       </form>
     </>
