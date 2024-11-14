8b3985e7fef8ae50b0403fdda9313caf8720cc8a
mickey-vee <171717823+mickey-vee@users.noreply.github.com>
2024-11-14 13:30:45 -0500

    Changes for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/big2-points/",
+  base: "/Big2-Points-Calculator/",
 });
